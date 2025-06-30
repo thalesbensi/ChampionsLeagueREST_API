834dc92685e8260caf577d63d4f21a4c2386e59f
thalesbensi <thalesbensi@outlook.com>
2025-06-30 18:31:53 -0300

    feat: Add Create Player route at router

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import * as PlayerController from "./controllers/players-controller";
 const router = Router();
 
 router.get('/players', PlayerController.getAllPlayers);
+router.post('/players', PlayerController.postPlayer);
 
 router.get('/players/:id', PlayerController.getPlayerById);
 
