a86af078a5ac8e1ac52663e0388e5081b99f1ebe
thalesbensi <thalesbensi@outlook.com>
2025-06-30 19:06:41 -0300

    feat: Apply HttpHandler

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -2,32 +2,20 @@ import {Request, Response} from "express";
 import * as PlayerService from "../services/players-service";
 
 export const getAllPlayers = async (req: Request, res: Response) => {
-    const data = await PlayerService.findAllPlayers();
-    res.status(200).json(data);
+    const httpResponse = await PlayerService.findAllPlayers()
+    res.status(httpResponse.statusCode).json(httpResponse.body);
 }
 
 export const getPlayerById = async (req: Request, res: Response) => {
     const id = parseInt(req.params.id);
-    const data = await PlayerService.findPlayerById(id);
-    if (data) {
-        res.status(200).json(data);
-    } else
-        return res.status(404).send('Not Found');
+    const httpResponse = await PlayerService.findPlayerById(id);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
 }
 
 export const postPlayer = async (req: Request, res: Response) => {
-    try {
-        const player: PlayerModel = req.body;
-        const result = await PlayerService.createPlayer(player);
-
-        if (result) {
-            return res.status(201).json({ message: "Player successfully created!" });
-        } else {
-            return res.status(400).json({ message: "Bad Request" });
-        }
-    } catch (error) {
-        return res.status(500).json({ message: "Internal Server Error", error });
-    }
+    const bodyValue = req.body;
+    const httpResponse = await PlayerService.createPlayer(bodyValue);
+    if (httpResponse) res.status(httpResponse.statusCode).json(httpResponse.body);
 };
 
 
--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,15 +1,35 @@
 import * as PlayerRepository from "../repositories/players-repository";
 import {PlayerModel} from "../models/PlayerModel";
+import * as httpResponse from "../utils/HttpHandler"
 
 
 export const findAllPlayers = async () => {
-    return await PlayerRepository.findAllPlayers();
+    const data = await PlayerRepository.findAllPlayers();
+    let response = null;
+    if (data) {
+        response = await httpResponse.ok(data);
+    } else {
+        response = await httpResponse.noContent();
+    }
+    return response;
 };
 
-export const findPlayerById = async (id:number) => {
-   return await PlayerRepository.findPlayerById(id);
-}
+export const findPlayerById = async (id: number) => {
+    const data = await PlayerRepository.findPlayerById(id);
+    let response = null;
+    if (data) {
+        response = await httpResponse.ok(data);
+    } else {
+        response = await httpResponse.noContent();
+    }
+    return response;
+};
 
-export const createPlayer = async (player:PlayerModel) => {
-    return await PlayerRepository.createPlayer(player)
-}
+export const createPlayer = async (player: PlayerModel) => {
+    let response = null;
+    if (Object.keys(player).length !== 0) {
+        await PlayerRepository.createPlayer(player);
+        response = httpResponse.created(player);
+    } else response = await httpResponse.badRequest();
+    return response;
+};
