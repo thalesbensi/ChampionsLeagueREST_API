5652e6f309696890e67c6899ba0f710a70eda9b0
thalesbensi <thalesbensi@outlook.com>
2025-06-30 22:48:10 -0300

    feat: Implement delete Player method

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,10 @@ const router = Router();
 
 router.get('/players', PlayerController.getAllPlayers);
 router.post('/players', PlayerController.postPlayer);
+router.delete('/players', PlayerController.deletePlayer)
 
 router.get('/players/:id', PlayerController.getPlayerById);
+
+
 
 export default router;
