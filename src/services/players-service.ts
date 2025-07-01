7639c92ee1a9c046e2705cca1235793bcbaf00d2
thalesbensi <thalesbensi@outlook.com>
2025-06-30 22:48:24 -0300

    feat: Implement delete Player service

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -5,31 +5,32 @@ import * as httpResponse from "../utils/HttpHandler"
 
 export const findAllPlayers = async () => {
     const data = await PlayerRepository.findAllPlayers();
-    let response = null;
-    if (data) {
-        response = await httpResponse.ok(data);
-    } else {
-        response = await httpResponse.noContent();
-    }
-    return response;
+    return  httpResponse.ok(data);
 };
 
 export const findPlayerById = async (id: number) => {
-    const data = await PlayerRepository.findPlayerById(id);
     let response = null;
+    const data = await PlayerRepository.findPlayerById(id);
     if (data) {
         response = await httpResponse.ok(data);
     } else {
-        response = await httpResponse.noContent();
+        response = await httpResponse.notFound();
     }
     return response;
 };
 
 export const createPlayer = async (player: PlayerModel) => {
     let response = null;
-    if (Object.keys(player).length !== 0) {
+    if (player) {
         await PlayerRepository.createPlayer(player);
         response = httpResponse.created(player);
     } else response = await httpResponse.badRequest();
     return response;
 };
+
+export const deletePlayer = async (id: number) => {
+    let response = null;
+    await PlayerRepository.deletePlayer(id);
+    response = httpResponse.noContent();
+    return response;
+}
