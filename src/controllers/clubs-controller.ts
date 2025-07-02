e0e14ce6f7799e8315d1ca4694405c41d6d2e000
thalesbensi <thalesbensi@outlook.com>
2025-07-02 18:03:44 -0300

    feat: implement clubs controller

---
+++ src/controllers/clubs-controller.ts
@@ -0,0 +1,6 @@
+import {Request, Response} from 'express';
+import * as ClubService from'"../services/clubs-service"
+
+export const getAllClubs = (req: Request, res: Response) => {
+    const response = await ClubService.getClubs();
+}
