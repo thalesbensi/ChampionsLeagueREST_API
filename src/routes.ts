7222e73f02794a7c4bd07c48b5282c34463152fc
thalesbensi <thalesbensi@outlook.com>
2025-07-02 18:03:33 -0300

    feat: implement updatePlayer route and club route

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,10 @@ import * as PlayerController from "./controllers/players-controller";
 const router = Router();
 
 router.get('/players', PlayerController.getAllPlayers);
-router.post('/players', PlayerController.postPlayer);
-router.delete('/players/:id', PlayerController.deletePlayer)
-
 router.get('/players/:id', PlayerController.getPlayerById);
+router.post('/players', PlayerController.postPlayer);
+router.patch('/players/:id', PlayerController.updatePlayer);
+router.delete('/players/:id', PlayerController.deletePlayer);
 
-
-
+router.get('/clubs', ClubController.getAllClubs);
 export default router;
